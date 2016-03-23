5ab53e172eb463c0492f00895d563aed3058c6ae
Paulo Pacitti <ppacitti@outlook.com>
2016-03-22 22:23:25 -0300

    design1
    teste

--- App1/App1/MainActivity.cs
+++ App1/App1/MainActivity.cs
@@ -16,15 +16,14 @@ namespace App1
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+            Window.RequestFeature(WindowFeatures.NoTitle);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
         }
     }
 }
--- App1/App1/Resources/Resource.Designer.cs
+++ App1/App1/Resources/Resource.Designer.cs
@@ -60,8 +60,11 @@ namespace App1
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050001
+			public const int button1 = 2131034113;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int textView1 = 2131034112;
 			
 			static Id()
 			{
